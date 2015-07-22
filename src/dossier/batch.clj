8de9de32391d059b946a7db2df8aa94779d192b3
Priyatam Mudivarti <priyatam@gmail.com>
2015-07-21 17:52:14 -0700

    remove dynamic vars
    - use args for aws-config

--- src/dossier/batch.clj
+++ src/dossier/batch.clj
@@ -1,19 +1,14 @@
 (ns dossier.batch
   (:refer-clojure :exclude [name parents])
   (:use [clojure.string :only (join split)])
-  (:require [environ.core :refer [env]]
-            [clojure.string :as str]
+  (:require [clojure.string :as str]
             [clojure.java.io :as io]
             [aws.sdk.s3 :as s3]
             [dossier.utils :refer :all]
             [zendown.core :as zen])
   (:import  [java.io File]))
 
-(def ^:dynamic *aws*
-  {:access-key (env :aws-access-key)
-   :secret-key (env :aws-secret-key)})
-
-(defn ls [dir]
+(defn ls [{:keys [access-key secret-key]} dir]
   "List files in dir"
   (doseq [file (file-seq (File. dir))] (println file)))
 
@@ -29,26 +24,26 @@
         :encoding "UTF-8"
         :append true))
 
-(defn upload [bucket doc io-type]
+(defn upload [aws-config bucket doc io-type]
   "Upload content to S3"
   (let [data (zen/readany :resource doc)
         url (generate-uri data)
         contents (fetch io-type doc)]
-    (s3/put-object *aws* bucket url contents)
+    (s3/put-object aws-config bucket url contents)
     url))
 
-(defn grant-read-access [bucket url]
+(defn grant-read-access [aws-config bucket url]
   "Grant all users a read permission"
-  (s3/update-object-acl *aws* bucket url (s3/grant :all-users :read)))
+  (s3/update-object-acl aws-config bucket url (s3/grant :all-users :read)))
 
-(defn exists? [bucket url]
+(defn exists? [aws-config bucket url]
   "Does content exist?"
-  (s3/object-exists? *aws* bucket url))
+  (s3/object-exists? aws-config bucket url))
 
-(defn download [bucket url]
+(defn download [aws-config bucket url]
   "Download content from S3 and return a string"
-  (slurp (:content (s3/get-object *aws* bucket url))))
+  (slurp (:content (s3/get-object aws-config bucket url))))
 
 (defn delete
-  [bucket url fname]
-  (s3/delete-object *aws* bucket url))
+  [aws-config bucket url]
+  (s3/delete-object aws-config bucket url))
--- test/dossier/batch_test.clj
+++ test/dossier/batch_test.clj
@@ -1,14 +1,19 @@
 (ns dossier.batch-test
-  (:require [clojure.test :refer :all]
+  (:require [environ.core :refer [env]]
+            [clojure.test :refer :all]
             [dossier.utils :refer :all]
             [zendown.core :as zen]
             [dossier.batch :as batch]))
 
 (def sample "samples/Hemingway-The-old-man-and-the-bridge.txt")
 
+(defn aws []
+  {:access-key (:aws-access-key env)
+   :secret-key (:aws-secret-key env)})
+
 (deftest test-batch
   (testing "batch"
     (let [bucket "dossier-test"
-          url (batch/upload bucket sample :resource)]
+          url (batch/upload (aws) bucket sample :resource)]
       (is (contains?
-           (zen/readany :str (batch/download bucket url)) :title)))))
+           (zen/readany :str (batch/download (aws) bucket url)) :title)))))
